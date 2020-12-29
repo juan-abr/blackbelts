a2a2cbebde840fc6f46d2d47615eff71399a16ea
senor-green-hill <juanabarro@protonmail.com>
2020-12-29 14:11:30 -0500

    BlackBelts model added to admin

--- admin.py
+++ admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .modles import *
 
 # Register your models here.
+admin.site.register(BlackBelts)
--- models.py
+++ models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class BlackBelts (models.Model):
+    prefix          = models.CharField(max_length = 10)
+    first_name      = models.CharField(max_length = 20)
+    last_name       = models.CharField(max_length = 20)
+    rank            = models.CharField(max_length = 20)
+    instructor_bio  = models.TextField(blank = True)
