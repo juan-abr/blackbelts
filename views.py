a3ffbbdbac5c4711c777fa219c6a40db627bed63
senor-green-hill <juanabarro@protonmail.com>
2020-12-31 15:41:40 -0500

    blackbelt_list.html added

--- views.py
+++ views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import BlackBelts
 
 # Create your views here.
+class BlackBeltsListView(ListView):
+    model = BlackBelts
+
+class BlackBeltsDetailView(DetailView):
+    model = BlackBelts
+    pk_url_kwarg = 'blackbelts_id'
