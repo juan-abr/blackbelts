6b290bb9d92f6bfe646c8ba58e176998c55ee19d
senor-green-hill <juanabarro@protonmail.com>
2021-01-03 14:24:44 -0500

    image_url added

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class BlackBelt (models.Model):
     last_name       = models.CharField(max_length = 20)
     rank            = models.CharField(max_length = 20)
     instructor_bio  = models.TextField(blank = True)
+    image_url       = models.CharField(max_length = 20, blank = True)
 
     def __str__(self):
         return self.prefix + " " + self.first_name + " " + self.last_name
