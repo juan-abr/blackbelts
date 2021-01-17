e14f70d6feb9244704a8c9442ea25e714ef91fd8
senor-green-hill <juanabarro@protonmail.com>
2021-01-17 15:49:21 -0500

    blackbelt_detail.html added

--- urls.py
+++ urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', BlackBeltListView.as_view(), name='black_belt_list_view'),
+    path('<int:blackbelt_id>', BlackBeltDetailView.as_view(), name='black_belt_detail_view'),
 ]
