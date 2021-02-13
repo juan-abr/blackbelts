f2fc1fed124dae741e19a65194b32322ba4e6ea7
senor-green-hill <juanabarro@protonmail.com>
2021-02-13 15:31:04 -0500

    instructors sorted by prefix

--- models.py
+++ models.py
@@ -22,4 +22,4 @@ class BlackBelt (models.Model):
     image_url       = models.CharField(max_length = 20, blank = True)
 
     def __str__(self):
-        return self.prefix + " " + self.first_name + " " + self.last_name
+        return self.first_name + " " + self.last_name
